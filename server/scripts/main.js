47c47b9db8dd160302e0c9d2b9619445f2c5fbc4
Nic Schumann <nicolas_schumann@brown.edu>
2019-04-29 18:41:35 -0700

    [trivial] tweaks visualization parameters

--- server/scripts/main.js
+++ server/scripts/main.js
@@ -5,14 +5,14 @@ import { CasesByDay } from './chart-cases-by-day.js';
 
 livereload();
 
-const tsvFilename = 'cases_by_decision_date_2000_2017.tsv';
+const tsvFilename = 'cases_by_decision_date_1600_1900.tsv';
 
 // 612 x 796 = 8.5 x ll
 // 796 x 1224 = 11 x l7
 // 1758 x 2592 = 24 x 36
 // 3516 x 5184 = 48 x 72
-const width = 612;
-const height = 796;
+const width = 796;
+const height = 1224;
 
 const chart = new CasesByDay( tsvFilename, width, height );
 
