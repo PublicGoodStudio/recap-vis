cc06cf900cd652efb0162a836454313e060cfd63
Matthew Phillips <matthewphillips@Matthews-MacBook-Pro.local>
2019-05-14 14:56:59 -0700

    added basic coloring logic to grid of geos

--- server/scripts/grid-cases-by-geo.js
+++ server/scripts/grid-cases-by-geo.js
@@ -2,13 +2,7 @@
 
 var d3 = Object.assign( require('d3'), require('d3-fetch'), require('d3-scale-chromatic'), require('d3-geo') );
 var moment = require('moment');
-
 var topojson = require('topojson');
-var projection;
-var path;
-
-
-//import { daysInYear, dayOfYear, monthNames } from './utility-day-of-year.js';
 
 class CasesByGeo {
 
@@ -40,17 +34,12 @@ class CasesByGeo {
                 .attr('height', height + 'px');
 
 
-
-        path   = d3.geoPath()
-                   .projection(this.scale(.15));
-
     }
 
     generateVisualization() {
 
-      var jurisdiction_data_files = [];
-
       d3  .json('/static/data/' + this.jsonFilePath )
+          .then( this.getMaxCaseCount.bind(this) )
           .then( this.renderTextDesc.bind(this) );
 
       d3  .json( "https://d3js.org/us-10m.v1.json" )
@@ -66,6 +55,7 @@ class CasesByGeo {
 
       // increase of n cases
       // n cases total
+
 
       const ranges = [
         {start_year: 1657, end_year: 1657}, {start_year: 1658, end_year: 1659},
@@ -93,9 +83,8 @@ class CasesByGeo {
       var row_position = 0;
       var column_position = 0;
       var case_count = 0;
-      for (var range in ranges) {
-        //console.log('getting ranges for ', ranges[range].start_year, ranges[range].end_year )
-        var ranged_data = this.get_counts( data, ranges[range].start_year, ranges[range].end_year );
+      for (let range in ranges) {
+        let ranged_data = this.get_counts( data, ranges[range].start_year, ranges[range].end_year );
 
         case_count = case_count + ranged_data.case_count;
         this.svg
@@ -135,13 +124,13 @@ class CasesByGeo {
 
         var bundle = {};
         var case_count = 0;
-        for ( var d in data ) {
+        for ( let d in data ) {
 
           var case_year = parseInt(d, 10);
 
           if (case_year >= start_year && case_year <= end_year) {
 
-            for (var jurisdiction in data[d]) {
+            for (let jurisdiction in data[d]) {
 
               if (jurisdiction in bundle) {
                   bundle[jurisdiction] = bundle[jurisdiction] + data[d][jurisdiction];
@@ -156,37 +145,110 @@ class CasesByGeo {
           }
 
         }
-
+        console.log(case_count);
         return {case_count: case_count, bundle: bundle};
     }
 
-    scale (scaleFactor) {
-        return d3.geoTransform({
-            point: function(x, y) {
-                this.stream.point(x * scaleFactor, y  * scaleFactor);
+    getMaxCaseCount(data) {
+
+      var bundle = {};
+      for ( let d in data ) {
+
+          for (let jurisdiction in data[d]) {
+
+            if (jurisdiction in bundle) {
+                bundle[jurisdiction] = bundle[jurisdiction] + data[d][jurisdiction];
+            } else {
+              bundle[jurisdiction] = data[d][jurisdiction];
             }
-        });
-    }
+
+          }
+
+
+        }
+
+
+        this.maxCaseCount = d3.max(d3.values(bundle));
+        //console.log(this.maxCaseCount)
+
+
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     renderGeo(data) {
 
+      function scale (scaleFactor) {
+          return d3.geoTransform({
+              point: function(x, y) {
+                  this.stream.point(x * scaleFactor, y  * scaleFactor);
+              }
+          });
+      }
 
-      var row_position = 0;
-      var column_position = 0;
-      var x = 0;
-      var y = 0;
+      function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+      }
 
-      for (var i = 0; i < 38; i++) { // match our decade descriptions
+
+      var getCountsFromId = (stateId) => {
+        d3  .json('/static/data/' + this.jsonFilePath )
+            .then(
+
+            function(data) {
+              console.log(stateId);
+
+            }
+
+
+          )
+      }
+
+      let path   = d3.geoPath()
+                 .projection(scale(.15));
+
+      let color = d3.scaleLinear()
+         .domain([1, 101])
+         .range(["white", "steelblue"]);
+
+      let row_position = 0;
+      let column_position = 0;
+      let x = 0;
+      let y = 0;
+
+      for (let i = 0; i < 38; i++) { // match our decade descriptions
 
         x = 100 + (300 * column_position);
         y = 100 + (300 * row_position );
 
         this.svg.append("g")
             .attr("class", "states-choropleth")
-          .selectAll("path")
-          .data(topojson.feature(data, data.objects.states).features)
-          .enter()
-          .append("path")
+            .selectAll("path")
+            .data(topojson.feature(data, data.objects.states).features)
+            .enter()
+            .append("path")
+            .attr("fill", function(d) {
+                getCountsFromId(d.id);
+            // make function that accepts d.id, maps it to cap state abrev.
+            // and then returns the count for that state
+            // we then pass that value to our color function
+
+            return color(getRandomInt(1,100));
+
+          })
           .attr('transform', 'translate(' + x + ',' + y + ')')
            .attr("d", path);
 
@@ -196,8 +258,6 @@ class CasesByGeo {
             .attr('transform', 'translate(' + 100 + (300 * column_position) + ',' + 100 + (300 * row_position )  + ')')
             .attr('transform', 'translate(' + x + ',' + y + ')')
             .attr("d", path(topojson.mesh(data, data.objects.states, function(a, b) { return a !== b; })));
-
-
 
 
         column_position = column_position + 1;
